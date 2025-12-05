f1f9c1d4893385c88b7da67e16bb5f9ee18353b5
Annelepus <annelepus@gmail.com>
2025-12-05 06:17:54 +0200

    Adjust navigation functions for main margin

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "10%";
-  document.getElementById("main").classList.add("shifted");
+  document.getElementById("main").style.marginLeft = "10%";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").classList.remove("shifted");
+  document.getElementById("main").style.marginLeft= "0";
   document.body.style.backgroundColor = "white";
 }
