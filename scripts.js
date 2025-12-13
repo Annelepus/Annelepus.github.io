de6c4579b2123966a791d858f39ac68001303e5c
Annelepus <annelepus@gmail.com>
2025-12-14 00:31:09 +0200

    Replacement of files

--- scripts.js
+++ scripts.js
@@ -8,25 +8,25 @@ function openNav() {
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-  document.getElementById("lore").style.marginLeft= "0";
-  document.getElementById("buttontrans").style.marginLeft = "0%";  
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("lore").style.marginLeft = "0";
+  document.getElementById("buttontrans").style.marginLeft = "0%";
   document.body.style.backgroundColor = "rgba(100, 100, 100, 0.4)";
 }
 
 function myTooltip(e) {
-    const tooltip = document.getElementById("tooltip");
+  const tooltip = document.getElementById("tooltip");
 
-    tooltip.style.left = (e.clientX + 10) + "px"; 
-    tooltip.style.top = (e.clientY + 10) + "px";
+  tooltip.style.left = (e.clientX + 10) + "px";
+  tooltip.style.top = (e.clientY + 10) + "px";
 }
 
 function showTip() {
-    document.getElementById("tooltip").style.opacity = "1";
+  document.getElementById("tooltip").style.opacity = "1";
 }
 
 function hideTip() {
-    document.getElementById("tooltip").style.opacity = "0";
+  document.getElementById("tooltip").style.opacity = "0";
 }
 
 const toggleTo2 = document.getElementById("toggle-to-2");
@@ -54,3 +54,21 @@ toggleTo1.addEventListener("click", () => {
   show(toggleTo2);
   show(div1);
 });
+
+function changeImage() {
+    var img = document.getElementById("image");
+
+    // Fade out
+    img.style.opacity = 0;
+
+    // Setting a timeout specifications for the function, mostly extra.
+    setTimeout(function() {
+        if (img.src.includes("Endministrator_Splash_Art.png")) {
+            img.src = "/images/Operators/Endmin/EndministratorM_Splash_Art.png";
+        } else {
+            img.src = "/images/Operators/Endmin/Endministrator_Splash_Art.png";
+        }
+        // Fade in
+        img.style.opacity = 1;
+    }, 500); // 500 ms transition between the two images
+}
